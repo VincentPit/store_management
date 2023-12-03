c435ce37d59134839bd5bd16ce9abf1e2b99f29f
sw4751 <152218686+sw4751@users.noreply.github.com>
2023-12-03 16:27:10 -0500

    Update InventoryManager.java

--- models/InventoryManager.java
+++ models/InventoryManager.java
@@ -5,13 +5,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 public class InventoryManager extends User implements Serializable{
-    public InventoryManager(String name, String staffCode) {
-        super(name, staffCode);
+    public InventoryManager(String name, String staffCode, String passWord) {
+        // Automatically setting the type to "BusinessOwner"
+        super(name, staffCode, passWord, "InventoryManager");
     }
 
-    public void buyProducts(String name, int unitCost, int quantity) {
+    //buyProduct update stockLevel and unitCost, the new purchase price could be different
+    //if the price is different, it re-calculate average unitCost, and update quanity
+    public void buyProducts(String name, double unitCost, int quantity) {
+        Merchandise tempMerchandise = store.findMerchandise(name);
+        double tempCost = tempMerchandise.getUnitCost();
+        int tempStockLevel =  tempMerchandise.getStockLevel();
+
+        int newStockLevel = tempMerchandise.getStockLevel() + quantity;
+        double newCost = ((unitCost * quantity) + (tempCost * tempStockLevel))/newStockLevel;
+        tempMerchandise.setStockLevel(newStockLevel);
+        tempMerchandise.setUnitCost(newCost);
+
     }
 
+    //get the list with all Merchandise
     public void viewAll() {
+        List<Merchandise> tempMerchandiseList = viewAllMerchandise();
     }
 }
