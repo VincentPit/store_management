92dad43c2f1df0e38c1049043266a8891484cf08
sw4751 <152218686+sw4751@users.noreply.github.com>
2023-12-03 16:58:42 -0500

    Update BusinessOwner.java

--- models/Merchandise.java
+++ models/Merchandise.java
@@ -6,13 +6,16 @@ import java.util.List;
 import java.util.Map;
 class Merchandise implements Serializable {
     private String name;
-    private int unitCost;
-    private int unitPrice;
+    private double unitCost;
+    private double unitPrice;
+    private int stockLevel;
+    
 
-    public Merchandise(String name, int unitCost, int unitPrice) {
+    public Merchandise(String name, double unitCost, double unitPrice, int stockLevel ) {
         this.name = name;
         this.unitCost = unitCost;
         this.unitPrice = unitPrice;
+        this.stockLevel = stockLevel;
     }
 
     // Getters for name, unitCost, and unitPrice
