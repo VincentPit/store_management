8b06de33e3d23dc9d7e676ef6b38086e977dd4d4
sw4751 <152218686+sw4751@users.noreply.github.com>
2023-12-03 16:01:03 -0500

    Update BusinessOwner.java

--- models/BusinessOwner.java
+++ models/BusinessOwner.java
@@ -1,18 +1,48 @@
 package models;
 import java.io.*;
+import models.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 class BusinessOwner extends User implements Serializable {
 
-    public BusinessOwner(String name, String staffCode) {
-        super(name, staffCode);
+    public BusinessOwner(String name, String staffCode, String passWord) {
+        // Automatically setting the type to "BusinessOwner"
+        super(name, staffCode, passWord, "BusinessOwner");
     }
 
-    public void addNewInventory(String name, int unitCost, int unitPrice) {
+    public void addNewMerchandise(String name, double unitCost, int unitPrice, int stockLevel) {
+        Merchandise newMerchandise = new Merchandise(name, unitCost, unitPrice, stockLevel);
+        store.addMerchandise(newMerchandise);
     }
 
-    public void setDiscount(String name, double discountRate) {
+    public void addSalesStaff(String name, String staffCode, String passWord)
+    {   //if none of the user share the same staffCode then allow add user using this staffCode
+        if(!store.findUserByStaffCode(staffCode))
+        {
+            SalesStaff newSalesStaff = new SalesStaff(name, staffCode, passWord);
+            store.addUser(newSalesStaff);
+        }
+
     }
+
+    public void addInventoryManager(String name, String staffCode, String passWord)
+    {   //if none of the user share the same staffCode then allow add user using this staffCode
+        if(!store.findUserByStaffCode(staffCode))
+        {
+            SalesStaff newInventoryManager = new SalesStaff(name, staffCode, passWord);
+            store.addUser(newInventoryManager);
+        }
+    }
+
+    public void deleteStaff(String staffCode)
+    {
+        store.deleteUser(staffCode);
+    }
+
+    public void setDiscount(String name, double price) {
+        store.setPrice(name, price);
+    }
+    
 }
