9c7ace6e29c2d503ed0e294b16a846eae1d125e3
Stephen Li <jl13122@nyu.edu>
2023-12-08 01:59:02 -0500

    MainGUI for Staff

---
+++ models/interfaces/SalesStaffGUI.java
@@ -0,0 +1,83 @@
+package models.interfaces;
+import java.time.*;
+import models.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+
+
+public class SalesStaffGUI {
+
+    private SalesStaff salesStaff;
+    private JFrame frame;
+
+    public SalesStaffGUI(SalesStaff salesStaff) {
+        this.salesStaff = salesStaff;
+
+        frame = new JFrame("SalesStaff Main Menu");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel helloLabel = new JLabel("Hello " + salesStaff.getName());
+        helloLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        JButton goToTransactionButton = new JButton("Go to Transaction");
+        goToTransactionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openSalesStaffTransactionGUI();
+            }
+        });
+
+        JButton goToMerchandiseButton = new JButton("Go to Merchandise");
+        goToMerchandiseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openSalesStaffMerchandiseGUI();
+            }
+        });
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
+        buttonsPanel.add(goToTransactionButton);
+        buttonsPanel.add(goToMerchandiseButton);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(helloLabel, BorderLayout.NORTH);
+        mainPanel.add(buttonsPanel, BorderLayout.CENTER);
+
+        frame.add(mainPanel);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private void openSalesStaffTransactionGUI() {
+        Store store = new Store();
+        LocalDate date = LocalDate.now();
+        User user = store.getUserList().get(0);
+        SalesStaff salesStaff = new SalesStaff(user.getName(), user.getStaffCode(), store, user.getPwd(), date);
+        SwingUtilities.invokeLater(() -> new SalesStaffGUI_Transaction(salesStaff));
+
+        frame.dispose();
+    }
+
+    private void openSalesStaffMerchandiseGUI() {
+        Store store = new Store();
+        LocalDate date = LocalDate.now();
+        User user = store.getUserList().get(0);
+        SalesStaff salesStaff = new SalesStaff(user.getName(), user.getStaffCode(), store, user.getPwd(), date);
+
+        SwingUtilities.invokeLater(() -> new SalesStaffGUI_Merchandises(salesStaff));
+
+        frame.dispose();
+    }
+
+    public static void main(String[] args) {
+        Store store = new Store();
+        LocalDate date = LocalDate.now();
+        User user = store.getUserList().get(0);
+        SalesStaff salesStaff = new SalesStaff(user.getName(), user.getStaffCode(), store, user.getPwd(), date);
+
+        SwingUtilities.invokeLater(() -> new SalesStaffGUI(salesStaff));
+    }
+}
